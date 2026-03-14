1cf9693c22c1ca2ec856978ec189d221ba225a10
Matouš Ettler <62652758+matousettler@users.noreply.github.com>
2026-03-14 20:37:32 +0100

    Update menu.js

--- api/menu.js
+++ api/menu.js
@@ -2,10 +2,12 @@ export default async function handler(req, res) {
   const url = 'https://www.strava.cz/strava5/Jidelnicky/XML?zarizeni=1806';
   try {
     const response = await fetch(url);
-    const data = await response.text();
-    res.setHeader('Content-Type', 'application/xml');
+    const buffer = await response.arrayBuffer();
+    
+    // Pošleme surová data, prohlížeč si kódování windows-1250 přebere sám
+    res.setHeader('Content-Type', 'text/xml; charset=windows-1250');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).send(data);
+    res.status(200).send(Buffer.from(buffer));
   } catch (error) {
     res.status(500).json({ error: 'Chyba při stahování dat' });
   }
