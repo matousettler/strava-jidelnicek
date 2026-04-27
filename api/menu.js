82f782fe995274b2af22b498fe4dca01d7edd37e
Matouš Ettler <62652758+matousettler@users.noreply.github.com>
2026-04-27 09:00:51 +0200

    Update menu.js

--- api/menu.js
+++ api/menu.js
@@ -1,12 +1,22 @@
 export default async function handler(req, res) {
-  const url = 'https://www.strava.cz/strava5/Jidelnicky/XML?zarizeni=1806';
+  const url1806 = 'https://www.strava.cz/strava5/Jidelnicky/XML?zarizeni=1806';
+  const url0398 = 'https://www.strava.cz/strava5/Jidelnicky/XML?zarizeni=0398';
+
   try {
-    const response = await fetch(url);
-    const data = await response.text();
-    res.setHeader('Content-Type', 'text/xml; charset=utf-8');
+    const [res1, res2] = await Promise.all([
+      fetch(url1806).then(r => r.text()),
+      fetch(url0398).then(r => r.text())
+    ]);
+
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).send(data);
+    
+    // Pošleme obě XML jako jeden JSON objekt
+    res.status(200).json({
+      xml1806: res1,
+      xml0398: res2
+    });
   } catch (error) {
-    res.status(500).json({ error: 'Chyba API' });
+    res.status(500).json({ error: 'Chyba při stahování dat z jídelen' });
   }
 }
