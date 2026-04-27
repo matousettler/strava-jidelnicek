bbb8b3600d52ad8e59d7b5415bc9ac5408bf2f5e
Matouš Ettler <62652758+matousettler@users.noreply.github.com>
2026-04-27 15:20:41 +0200

    Update screenshot.js

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,15 +1,32 @@
 export const config = { runtime: 'edge' };
 
 export default async function handler(req) {
-  const url = 'https://api.screenshotone.com/take?access_key=cgw6R8brZUbS7Q&url=https%3A%2F%2Fpadlet.com%2Fembed%2Fxet6al33uutzs6wl&device_scale_factor=3&viewport_device=ipad_landscape&format=png&block_ads=true&block_cookie_banners=true&block_banners_by_heuristics=false&block_trackers=true&delay=0&timeout=60&response_type=by_format&full_page=true&image_quality=80';
+  const params = new URLSearchParams({
+    access_key: 'cgw6R8brZUbS7Q',
+    url: 'https://padlet.com/embed/xet6al33uutzs6wl',
+    viewport_width: 1280,
+    viewport_height: 720,
+    device_scale_factor: 3,
+    viewport_device: 'ipad_landscape',
+    format: 'png',
+    block_ads: true,
+    block_cookie_banners: true,
+    block_banners_by_heuristics: false,
+    block_trackers: true,
+    delay: 0,
+    timeout: 60,
+    response_type: 'by_format',
+    full_page: true,
+    image_quality: 80,
+  });
 
-  const response = await fetch(url);
+  const response = await fetch(`https://api.screenshotone.com/take?${params}`);
   const image = await response.arrayBuffer();
 
   return new Response(image, {
     headers: {
       'Content-Type': 'image/png',
-      'Cache-Control': 'public, max-age=1800', // cache 30 minut
+      'Cache-Control': 'public, max-age=1800',
     },
   });
 }
